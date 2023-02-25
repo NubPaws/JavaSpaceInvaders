da6e5ef10b538425b01d002ad031f145a0143295
NubPaws <dxroomram@gmail.com>
2023-02-25 20:21:19 +0200

    Documented the helper class

--- src/spaceinvaders/utils/Helper.java
+++ src/spaceinvaders/utils/Helper.java
@@ -1,11 +1,28 @@
 package spaceinvaders.utils;
 
+/**
+ * General helper functions class.
+ */
 public final class Helper {
 	
+	/**
+	 * Clamps the current float value between a max and a min values.
+	 * @param cur the current value.
+	 * @param min the minimal value.
+	 * @param max the maximal value.
+	 * @return the clamped value.
+	 */
 	public static float clampf(float cur, float min, float max) {
 		return (cur < min) ? min : (cur > max) ? max : cur;
 	}
 	
+	/**
+	 * Clamps the current int value between a max and a min values.
+	 * @param cur the current value.
+	 * @param min the minimal value.
+	 * @param max the maximal value.
+	 * @return the clamped value.
+	 */
 	public static int clampi(int cur, int min, int max) {
 		return (cur < min) ? min : (cur > max) ? max : cur;
 	}
