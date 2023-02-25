38486c1224308ca326f0db005c2bf1242fc2fef8
NubPaws <dxroomram@gmail.com>
2023-02-25 20:19:45 +0200

    Ordered Barrier and Enemy

--- src/spaceinvaders/objects/Barrier.java
+++ src/spaceinvaders/objects/Barrier.java
@@ -3,15 +3,28 @@ package spaceinvaders.objects;
 import spaceinvaders.handlers.Handler;
 import spaceinvaders.handlers.SoundHandler;
 
+/**
+ * Class for the instances of the barriers that protect the player in the game.
+ */
 public class Barrier extends GameObject {
 	
-	private int deathTimer = 0;
-	private byte count = 1;
+	/* Timer for the death animation. */
+	private int deathTimer;
 	
+	/**
+	 * Initializes a barrier object at the designed location.
+	 * 
+	 * @param x the x coordinate.
+	 * @param y the y coordinate.
+	 * @param handler the handler of the game objects of the game.
+	 */
 	public Barrier(float x, float y, Handler<GameObject> handler) {
-		super(x, y, 0, 0, GameObjectID.Barrier1, handler);
-		width = getSprite().getWidth();
-		height = getSprite().getHeight();
+		super(x, y,
+			GameObjectID.Barrier1.image().getWidth(),
+			GameObjectID.Barrier1.image().getHeight(),
+			GameObjectID.Barrier1, handler);
+		
+		deathTimer = 0;
 	}
 	
 	@Override
@@ -23,26 +36,22 @@ public class Barrier extends GameObject {
 	
 	@Override
 	public void collided(GameObject with) {
-		count++;
-		if (count == 2) {
-			switch (id) {
-			case Barrier1:
-				id = GameObjectID.Barrier2;
-				break;
-			case Barrier2:
-				id = GameObjectID.Barrier3;
-				break;
-			case Barrier3:
-				id = GameObjectID.Barrier4;
-				break;
-			case Barrier4:
-				id = GameObjectID.BarrierExplosion;
-				SoundHandler.play("explode.aiff");
-				break;
-			default:
-				break;
-			}
-			count = 1;
+		switch (id) {
+		case Barrier1:
+			id = GameObjectID.Barrier2;
+			break;
+		case Barrier2:
+			id = GameObjectID.Barrier3;
+			break;
+		case Barrier3:
+			id = GameObjectID.Barrier4;
+			break;
+		case Barrier4:
+			id = GameObjectID.BarrierExplosion;
+			SoundHandler.play("explode.aiff");
+			break;
+		default:
+			break;
 		}
 	}
 	
--- src/spaceinvaders/objects/Enemy.java
+++ src/spaceinvaders/objects/Enemy.java
@@ -9,20 +9,40 @@ import spaceinvaders.main.GameBoard;
 import spaceinvaders.utils.Counter;
 import spaceinvaders.utils.Flag;
 
+/**
+ * Class for the enemies. This class handles all types of enimies.
+ */
 public class Enemy extends GameObject {
 	
+	/* General random generator for all of the enemies. */
 	private static final Random rand = new Random();
 	
+	/* Timer for the death of an entity. */
 	private short deathTimer = 0;
 	
+	/* Information from the player and of the game. */
 	private Counter score;
 	private Flag playerSprayAndPray;
 	private Counter playerSprayAndPrayTimer;
 	
-	public Enemy(float x, float y, GameObjectID id, Handler<GameObject> handler, Counter score, Player player) {
+	/**
+	 * Creates an instance of an enemy.
+	 * 
+	 * @param x the x coordinate.
+	 * @param y the y coordinate.
+	 * @param id the identifier of the enemy.
+	 * @param handler the handler for all of the game objects.
+	 * @param score the score counter of the game.
+	 * @param player the player instance of the game.
+	 */
+	public Enemy(float x, float y, GameObjectID id, Handler<GameObject> handler,
+				Counter score, Player player) {
 		super(x, y, 0, 0, id, handler);
 		width = getSprite().getWidth();
 		height = getSprite().getHeight();
+		/* If we are creating an enemy boss then it has a horizontal velocity unlike the rest of
+		 * the enemies.
+		 */
 		if (id == GameObjectID.EnemyBoss) {
 			dx = -1.75f;
 		}
@@ -82,10 +102,22 @@ public class Enemy extends GameObject {
 		}
 	}
 	
+	/**
+	 * Checks whether or not the instance received a shot that was
+	 * sent by an instance identified as a player.
+	 * 
+	 * @param shooterToCheck the shooter to check if it a player.
+	 * @return whether or not the instance received a shot that was sent by the player.
+	 */
 	private boolean wasShotByPlayer(GameObject shooterToCheck) {
 		return shooterToCheck.is(GameObjectID.Shot) && ((Shot)shooterToCheck).sender == GameObjectID.Player;
 	}
 	
+	/**
+	 * Try to shoot as an enemy, which is shooting at random, at only specific times.
+	 * 
+	 * @param tick the current tick of the game.
+	 */
 	private void tryToShoot(int tick) {
 		if ((tick == 37 || tick == 13) && rand.nextInt(100) == 37) {
 			handler.add(new Shot(x + (width / 2), y + height + 1, 5, handler, id));
