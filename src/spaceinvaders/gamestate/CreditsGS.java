b5de013f0a8a15ddde1ba8ae82d5ae28bc370970
NubPaws <dxroomram@gmail.com>
2023-02-25 16:36:21 +0200

    Commented and Refactored the Credits gamestate

--- src/spaceinvaders/gamestate/CreditsGS.java
+++ src/spaceinvaders/gamestate/CreditsGS.java
@@ -8,19 +8,58 @@ import java.util.Scanner;
 import spaceinvaders.handlers.Keys;
 import spaceinvaders.main.GameBoard;
 
+/**
+ * The credit game state to display the credits of the game.
+ * 
+ * The format of a credits text file is as follows:
+ * A line containing only a dot will be an empty line.
+ * A line containing an @ at the start will not be included.
+ * An empty line will be removed and truncated.
+ * Any other line will be treated as is.
+ */
 public class CreditsGS extends GameState {
 	
-	public CreditsGS(GameStateManager gsm) { super(gsm); }
+	private static float SLOW_SPEED = 0.5f;
+	private static float FAST_SPEED = 3f;
 	
+	/* The y value of the text object. */
 	private float y;
-	private Scanner scanner;
+	/* The speed of the scrolling text. */
+	private float dy;
+	/* The height of the text */
+	private float height;
+	/* The scanner to read the values from the text file. */
 	private LinkedList<String> credits;
 	
+	/**
+	 * {@inheritDoc}
+	 */
+	public CreditsGS(GameStateManager gsm) {
+		super(gsm);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void init() {
+		loadText("/street_creds.txt");
+		
 		y = GameBoard.HEIGHT + 50;
-		scanner = new Scanner(getClass().getResourceAsStream("/street_creds.txt"));
+		dy = 0.5f;
+		height = 2568; // MAGIC NUMBER.
+	}
+	
+	/**
+	 * Loads the text from the credits file into the memory.
+	 * Does so line by line using the format stated above.
+	 * 
+	 * @param file the file for the credits text.
+	 */
+	private void loadText(String file) {
 		credits = new LinkedList<>();
+		
+		Scanner scanner = new Scanner(getClass().getResourceAsStream(file));
 		String next;
 		while (scanner.hasNextLine()) {
 			next = scanner.nextLine();
@@ -31,14 +70,26 @@ public class CreditsGS extends GameState {
 		}
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void update(int tick) {
-		y -= .5f;
-		if (Keys.isPressed(Keys.Escape) || y <= -2568) {
+		if (Keys.isHeld(Keys.Space))
+			dy = FAST_SPEED;
+		else
+			dy = SLOW_SPEED;
+		
+		y -= dy;
+		
+		if (Keys.isPressed(Keys.Escape) || y <= -height) {
 			dispose();
 		}
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void render(Graphics2D g2d) {
 		g2d.setColor(Color.white);
@@ -47,6 +98,9 @@ public class CreditsGS extends GameState {
 		}
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void dispose() {
 		gsm.setState(GameStateID.Menu);
